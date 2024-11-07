27202b976330b6b46d82bb391eba4443c0db9850
reon <reonsamuel2001@gmail.com>
2024-11-06 21:58:55 -0400

    Implemented landing page layout and styles

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-/* Add your JavaScript to this file */
+document.querySelector('.subscribe-button').addEventListener('click', function(e) {
+    e.preventDefault();
+    const email = document.querySelector('.email-input').value;
+    const messageDiv = document.querySelector('.message');
+    
+    if (email) {
+      messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+    } else {
+      messageDiv.textContent = 'Please enter a valid email address.';
+    }
+  });
+  
